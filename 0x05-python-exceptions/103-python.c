321f0fe76f681a2980ed6f35ef50f3800bbeefd2
Princess-Nnamdi <pnnamdi20200@gmail.com>
2023-07-31 04:49:17 -0700

    advanced tasks

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,102 @@
+/*
+ * File: 103-python.c
+ * Auth: Princess Nnamdi
+ */
+
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
+void print_python_list(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;	
+	PyObject *obj;
+
+	printf("[*] Python list info\n");
+
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+	obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
+	}
+}
+
+
+/**
+ * print_python_bytes - Prints basic info about Python byte objects.
+ * @p: A PyObject byte object.
+ */
+void print_python_bytes(PyObject *p)
+{
+	char *string;
+	long int i, size, limit;
+
+	printf("[.] bytes object info\n");
+
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	size = PyBytes_GET_SIZE(p);
+	string = PyBytes_AsString(p);
+
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
+
+	if (size >= 10)
+		limit = 10;
+	else
+		limit = size + 1;
+
+	printf("  first %ld bytes:", limit);
+
+	for (i = 0; i < limit; i++)
+	{
+		printf(" %02x", (unsigned char)string[i]);
+	}
+
+	printf("\n");
+}
+
+
+/**
+ * print_python_float - Prints basic info about Python float objects.
+ * @p: A PyObject float object.
+ */
+void print_python_float(PyObject *p)
+{
+	double val;
+	
+	printf("[.] float object info\n");
+
+	if (!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+
+	val = PyFloat_AS_DOUBLE(p);
+	printf("  value: %f\n", val);
+}
