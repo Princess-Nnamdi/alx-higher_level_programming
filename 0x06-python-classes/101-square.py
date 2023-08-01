2094fcca75d70144bc08400c896ddf3376cce3ef
Princess-Nnamdi <pnnamdi20200@gmail.com>
2023-08-01 05:58:23 -0700

    correction

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -91,11 +91,12 @@ class Square:
             print()
             return
 
-        for _ in range(self.__position[1]):
-            print()
-
-        for _ in range(self.__size):
-            print(" " * self.__position[0], "#" * self.__size)
+        
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
 
     def __str__(self):
         """
@@ -104,11 +105,11 @@ class Square:
         :return: The string representation of the square.
         :rtype: str
         """
-        result = ""
-        for _ in range(self.__position[1]):
-            result += "\n"
-
-        for _ in range(self.__size):
-            result += " " * self.__position[0] + "#" * self.__size + "\n"
-
-        return (result.strip())
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
